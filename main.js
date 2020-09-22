f425dbcb64667c9b1feb2e9542506ffe4578dad1
Owenthecoder1 <71456832+Owenthecoder1@users.noreply.github.com>
2020-09-22 12:06:00 -0700

    Got Text and Title Vars

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ alert("Success")
 }
 var con = document.getElementById("bCon")
 con.addEventListener("click",confunc)
+var title = document.getElementById("textbox")
+var title = document.getElementById("textbox2")
