c99a05bec828650957a63b23a78002db39eac1ac
Owenthecoder1 <71456832+Owenthecoder1@users.noreply.github.com>
2020-10-01 09:23:42 -0700

    Starting Dynamo Database

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 function confunc(){
-alert("Success")	
+	var text = document.getElementById("textbox2").value
+	var title = document.getElementById("textbox").value
+	if(title == "" || text == ""){
+		alert("Fail:nothing here")
+	}else{
+		alert("Success")
+		//var db = new AWS.DynamoDB()
+		console.log(title)
+	}
 }
 var con = document.getElementById("bCon")
 con.addEventListener("click",confunc)
-var title = document.getElementById("textbox")
-var text = document.getElementById("textbox2")
+
